cb6158506e8721e028d784b50672961711347711
Anil Dhurjaty <adhurjaty@gmail.com>
2022-12-01 21:35:14 -0800

    day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,11 @@
+﻿var inputFile = @"./input.txt";
+
+var output = File.ReadAllText(inputFile)
+    .Split("\n\n")
+    .Select(group => group
+        .Split("\n")
+        .Select(line => int.Parse(line))
+        .Sum())
+    .Max();
+
+Console.WriteLine(output);
