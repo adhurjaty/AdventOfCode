53b4d571aec0c063cb8707df86a91a82da80b069
Anil Dhurjaty <adhurjaty@gmail.com>
2022-12-04 17:05:54 -0800

    day4

---
+++ Day4/Solver.cs
@@ -0,0 +1,43 @@
+namespace AdventOfCode.Day4;
+
+public class Solver
+{
+    private readonly IEnumerable<string> _lines = File.ReadLines(@"./Day4/input.txt");
+
+    public int Part1()
+    {
+        return GetRanges()
+            .Count(rangePair => rangePair.Item2.Item2 <= rangePair.Item1.Item2);
+    }
+
+    public int Part2()
+    {
+        return GetRanges()
+            .Count(rangePair => rangePair.Item1.Item2 >= rangePair.Item2.Item1);
+    }
+
+    private IEnumerable<((int, int), (int, int))> GetRanges()
+    {
+        return _lines
+            .Select(line => line.Split(',')
+                .Select(part => part.Split('-').Select(x => int.Parse(x)).ToTuple())
+                .OrderBy(range => range.Item1)
+                .ThenByDescending(range => range.Item2)
+                .ToTuple());
+    }
+}
+
+public static class EnumerableExtensions
+{
+    public static (T, T) ToTuple<T>(this IEnumerable<T> lst)
+    {
+        try
+        {
+            return (lst.First(), lst.Skip(1).First());
+        }
+        catch (InvalidOperationException)
+        {
+            throw new Exception("List is too short");
+        }
+    }
+}
